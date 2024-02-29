9c6245b04d94cbfa865103761c38df3306a6275b
Ondrej Sika <ondrej@ondrejsika.com>
2024-02-29 08:38:50 +0100

    wip: 3

--- push.sh
+++ push.sh
@@ -6,7 +6,7 @@ patch_branch=master-public-patch-$(date +%s)
 # Construct the command to exclude files listed in .publicignore
 paths_to_exclude=$(awk '{print "--path " $0}' .publicignore | xargs)
 
-git switch $patch_branch
+git switch -c $patch_branch
 
 # Filter the current branch only
 git filter-repo --refs $patch_branch $paths_to_exclude --invert-paths --force
